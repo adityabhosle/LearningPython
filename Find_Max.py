ae000ec20cd20b4a6a43f4acffcf3e4f098d5cc2
Aditya Bhosle <adityabhsl83@gmail.com>
2023-06-21 11:44:45 +0530

    added whitespaces

--- Find_Max.py
+++ Find_Max.py
@@ -3,5 +3,5 @@ a = int(input("Enter a\n"))
 b = int(input("Enter b\n"))
 c = int(input("Enter c\n"))
 
-print("The maximum number from the above entered numbers is", max(a,b,c))
-print("The minimum number from the above entered numbers is", min(a,b,c))
+print("The maximum number from the above entered numbers is", max(a, b, c))
+print("The minimum number from the above entered numbers is", min(a, b, c))
